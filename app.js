be115332e8c2a1f0398ef5a39a18c8ea56d7a3ce
ALYSSA EVANS <ALYSSA.M.EVANS@GMAIL.COM>
2017-04-10 11:02:45 -0700

    Setup dev script and add favicon
    - Removed cookie-parser
    - Installed Nodemon
    - Added nodemon as npm run dev
    - Served a new favicon
    - Changed 'users' to 'movies'

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var movies = require('./routes/movies');
 
 var app = express();
 
@@ -15,15 +14,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/movies', movies);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Movie Life' });
+  res.render('index', { title: 'Lyssa\'s Movie Time' });
 });
 
 module.exports = router;
